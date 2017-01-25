1b91c382366265997b79a3fcbf8bccdad9d26460
CSE\dxz149 <dxz149@psu.edu>
2017-01-25 11:24:36 -0500

    Edits on the power system

--- Power.m
+++ Power.m
@@ -78,44 +78,51 @@ F_Max= 1.5.*F;
 %X_Attack=X;
 %attack_size= 1;
 a=0;
-my_AS=300;
-failed=zeros(my_AS,1)
-for as=1:20:my_AS
-    a=0;
-    X_Attack=X;
-    attack_size = as;
-    while a < attack_size
-        i= randi([1 length(X)],1,1);
-        j= randi([1 length(X)],1,1);
-        if X(i,j) ~=0
-            X_Attack(i,j)=0;
-            a=a+1;
+my_AS=100;
+repeat=100;
+failed=zeros(my_AS,repeat)
+for reap=1:repeat
+    for as=1:10:my_AS
+        a=0;
+        X_Attack=X;
+        attack_size = as;
+        while a < attack_size
+            i= randi([1 length(X)],1,1);
+            j= randi([1 length(X)],1,1);
+            if X(i,j) ~=0
+                X_Attack(i,j)=0;
+                a=a+1;
+            end
         end
-    end
 
-    Reactance_attack=zeros(N,N);
-    for i= 1:N
-        for j=1:N
-            if (i ==j)
-                for k=1:N
-                    if X_Attack(i,k) ~= 0
-                        Reactance_attack(i,j)= Reactance_attack(i,j)+(1./X_Attack(i,k));
+        Reactance_attack=zeros(N,N);
+        for i= 1:N
+            for j=1:N
+                if (i ==j)
+                    for k=1:N
+                        if X_Attack(i,k) ~= 0
+                            Reactance_attack(i,j)= Reactance_attack(i,j)+(1./X_Attack(i,k));
+                        end
                     end
-                end
-            else
-                if X_Attack(i,j) ~= 0
-                    Reactance_attack(i,j)= -1./X_Attack(i,j);
+                else
+                    if X_Attack(i,j) ~= 0
+                        Reactance_attack(i,j)= -1./X_Attack(i,j);
+                    end
                 end
             end
         end
+
+        F_Attack=zeros(N,1);
+        %F= Reactance*P;
+        F_Attack = Reactance_attack*power;
+
+        failed(as,reap) = sum(((F_Max- F_Attack) <0));
     end
-
-    F_Attack=zeros(N,1);
-    %F= Reactance*P;
-    F_Attack = Reactance_attack*power;
-
-    failed(as,1) = sum(((F_Max- F_Attack) <0))
 end
-failed
+Failures=zeros(my_AS,1)
+for k=1:repeat
+    Failures(:,1) =failed(:, k)+Failures(:,1);
+end
+Failures/repeat
 
 
